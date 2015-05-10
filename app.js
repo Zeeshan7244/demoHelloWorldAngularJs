b6e2c87e69da15874ea61336a09b676068735a6e
zeeshan7244 <k102169@nu.edu.pk>
2015-05-11 01:09:07 +0500

    commit

---
+++ app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Syed Zeeshan on 5/11/2015.
+ */
+
+
+
+var app = angular.module("myapp" , []);
+app.controller("HelloWorldCtrl" , function($scope){
+
+    $scope.name = "";
+
+});
